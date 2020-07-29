a24509eea015cf4a15f0489369bd6ae0b310c19c
Marco Evangelista <marcobjj@users.noreply.github.com>
2020-07-28 20:47:48 -0700

    adds animations and resume

---
+++ assets/javascript.js
@@ -0,0 +1,26 @@
+window.onload = function(e){ 
+
+
+document.getElementById('feature').addEventListener('click', function() {
+    window.open("https://marcobjj.github.io/module1/ ", '_blank');
+
+
+
+}, false);
+
+document.getElementById('run').addEventListener('click', function() {
+    window.open("https://marcobjj.github.io/run-buddy/ ", '_blank');
+
+
+
+}, false);
+
+
+document.getElementById('monaco').addEventListener('click', function() {
+    window.open("https://marcobjj.github.io/monaco", '_blank');
+
+
+
+}, false);
+
+}
