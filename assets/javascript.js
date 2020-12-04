93a0a0f2f7f9cf8385cd84795056b7d8e865df53
Marco Evangelista <marcobjj@users.noreply.github.com>
2020-12-03 23:37:46 -0800

    adds new bootcamptravel

--- assets/javascript.js
+++ assets/javascript.js
@@ -2,7 +2,7 @@ window.onload = function(e){
 
 
 document.getElementById('feature').addEventListener('click', function() {
-    window.open("https://stark-atoll-20406.herokuapp.com/ ", '_blank');
+    window.open("https://bootcamp-travel.herokuapp.com/ ", '_blank');
 
 
 
